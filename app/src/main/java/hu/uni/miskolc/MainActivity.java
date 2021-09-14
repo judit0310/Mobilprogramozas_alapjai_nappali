0425b030288c51791488a77f7771ea868ef7fd15
judit0310 <judit0310@gmail.com>
2021-09-14 15:35:43 +0200

    Második óra

--- app/src/main/java/hu/uni/miskolc/MainActivity.java
+++ app/src/main/java/hu/uni/miskolc/MainActivity.java
@@ -3,12 +3,18 @@ package hu.uni.miskolc;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.wtf(TAG, "Kiscica");
+        Log.d("Cica","Nagycica");
     }
+
+
 }
