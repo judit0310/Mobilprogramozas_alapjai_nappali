63d206ac70a27dd73cdff59803e6dd640b214ca4
Kunné Tamás Judit <judittamas@iit.uni-miskolc.hu>
2021-09-21 19:40:07 +0200

    Harmadik óra, layout és UI elemek.

--- app/src/main/java/hu/uni/miskolc/MainActivity.java
+++ app/src/main/java/hu/uni/miskolc/MainActivity.java
@@ -1,13 +1,19 @@
 package hu.uni.miskolc;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
+
+import java.time.LocalDateTime;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
 
+    @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
